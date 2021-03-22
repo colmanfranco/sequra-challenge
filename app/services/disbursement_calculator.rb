c19c487e1d0984e9dc048226667d12ba18584fb9
Franco Colman <colmanfm@gmail.com>
2021-03-22 12:58:05 -0300

    [GREEN] Adding use case for disbursed amount equals 301 changing fee amount to 0.85% of the amount

--- app/services/disbursement_calculator.rb
+++ app/services/disbursement_calculator.rb
@@ -1,5 +1,8 @@
 class DisbursementCalculator
   def execute(order)
+    if (order.amount).to_i > 300
+      return (order.amount).to_i * 0.0085
+      end
     if (order.amount).to_i >= 50
       return (order.amount).to_i * 0.0095
     end
