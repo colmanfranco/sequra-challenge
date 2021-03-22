c89f443ec82fe79766707c618ad263e32d4658e4
Franco Colman <colmanfm@gmail.com>
2021-03-22 12:54:59 -0300

    [GREEN] Adding use case for disbursed amount equals 300 changing fee amount

--- test/services/disbursement_test.rb
+++ test/services/disbursement_test.rb
@@ -55,6 +55,14 @@ class DisbursementTest < ActiveSupport::TestCase
     disbursedAmount = disbursementCalculator.execute(order)
 
     assert_equal ((order.amount).to_i * 0.0095), disbursedAmount, "Amounts dont match"
+    end
+
+  def test_disbursed_amount_should_be_zero_ninety_five_percent_of_three_hundred
+    order = get_test_order(300)
+    disbursementCalculator = DisbursementCalculator.new
+    disbursedAmount = disbursementCalculator.execute(order)
+
+    assert_equal ((order.amount).to_i * 0.0095), disbursedAmount, "Amounts dont match"
   end
 
 
