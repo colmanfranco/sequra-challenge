dd3e1fe475299ac5c075154164d1e12f60c37991
Franco Colman <colmanfm@gmail.com>
2021-03-22 17:35:53 -0300

    [RED] Test for when amount is a float number.

--- test/services/disbursements/disbursement_calculator_test.rb
+++ test/services/disbursements/disbursement_calculator_test.rb
@@ -6,82 +6,82 @@ class DisbursementCalculatorTest < ActiveSupport::TestCase
 
   def test_disbursed_amount_should_be_zero
     order = get_test_order(0)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
     assert_equal 0, disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_one_percent_of_one
     order = get_test_order(1)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.01), disbursed_amount, "Amounts dont match"
+    assert_equal (1 * 0.01), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_one_percent_of_twenty
     order = get_test_order(20)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.01), disbursed_amount, "Amounts dont match"
+    assert_equal (20 * 0.01), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_one_percent_of_forty_nine
     order = get_test_order(49)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.01), disbursed_amount, "Amounts dont match"
+    assert_equal (49 * 0.01), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_ninety_five_percent_of_fifty
     order = get_test_order(50)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0095), disbursed_amount, "Amounts dont match"
+    assert_equal (50 * 0.0095), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_ninety_five_percent_of_hundred
     order = get_test_order(100)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0095), disbursed_amount, "Amounts dont match"
+    assert_equal (100 * 0.0095), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_ninety_five_percent_of_two_hundred
     order = get_test_order(200)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0095), disbursed_amount, "Amounts dont match"
+    assert_equal (200 * 0.0095), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_ninety_five_percent_of_three_hundred
     order = get_test_order(300)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0095), disbursed_amount, "Amounts dont match"
+    assert_equal (300 * 0.0095), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_eighty_five_percent_of_three_hundred_one
     order = get_test_order(301)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0085), disbursed_amount, "Amounts dont match"
+    assert_equal (301 * 0.0085), disbursed_amount, "Amounts dont match"
   end
 
   def test_disbursed_amount_should_be_zero_eighty_five_percent_of_three_hundred_thirteen
     order = get_test_order(313)
-    disbursementCalculator = DisbursementCalculator.new
-    disbursed_amount = disbursementCalculator.execute(order)
+    disbursement_calculator = DisbursementCalculator.new
+    disbursed_amount = disbursement_calculator.execute(order)
 
-    assert_equal ((order.amount).to_i * 0.0085), disbursed_amount, "Amounts dont match"
+    assert_equal (313 * 0.0085), disbursed_amount, "Amounts dont match"
     end
 
   def test_disbursed_amount_should_be_zero_eighty_five_percent_of_float_number
