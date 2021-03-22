8159f39bfaa1a2bf0c18a046bc6d27b61ace73d1
Franco Colman <colmanfm@gmail.com>
2021-03-22 15:34:42 -0300

    [REF] Refactoring to extract a class Ranges for better readability.

--- app/services/disbursements/disbursement_calculator.rb
+++ app/services/disbursements/disbursement_calculator.rb
@@ -1,12 +1,13 @@
 class DisbursementCalculator
   def execute(order)
+    small_amount_fee = 0.01
     order_amount = (order.amount).to_i
     if order_amount > Ranges::SECOND.amount
       return order_amount * Ranges::SECOND.fee
-      end
+    end
     if order_amount >= Ranges::FIRST.amount
       return order_amount * Ranges::FIRST.fee
     end
-    order_amount * 0.01
+    order_amount * small_amount_fee
   end
 end
